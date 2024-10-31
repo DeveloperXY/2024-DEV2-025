86d8bb651fbb5aca5d8b7cbb7eab392238fd83ee
Mohammed Aouf ZOUAG <m.a.zouag@gmail.com>
2024-10-31 20:22:57 +0100

    Add failing test for placing a mark on the game board

--- app/src/test/java/com/developerxy/bnp_tic_tac_toe/TicTacToeGameBoardTests.kt
+++ app/src/test/java/com/developerxy/bnp_tic_tac_toe/TicTacToeGameBoardTests.kt
@@ -2,6 +2,7 @@ package com.developerxy.bnp_tic_tac_toe
 
 import com.developerxy.bnp_tic_tac_toe.domain.GameBoard
 import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.comparables.shouldBeEqualComparingTo
 import org.junit.Before
 import org.junit.Test
 
@@ -17,5 +18,14 @@ class TicTacToeGameBoardTests {
     @Test
     fun gameBoardIsInitiallyClearedOut() {
         gameBoard.isClear().shouldBeTrue()
+    }
+
+    @Test
+    fun gameBoardIsUpdatedUponPlacingMark() {
+        val coords = 1 to 1
+        val mark = "X"
+
+        gameBoard.placeMark(mark, at = coords)
+        gameBoard.markAt(coords).shouldBeEqualComparingTo(mark)
     }
 }
