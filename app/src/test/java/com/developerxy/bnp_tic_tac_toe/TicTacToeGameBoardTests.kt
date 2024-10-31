df27b2646f19160bb4af6db8a16a645e0c2a3f0d
Mohammed Aouf ZOUAG <m.a.zouag@gmail.com>
2024-10-31 20:36:53 +0100

    Add test to verify an exception is thrown upon placing a mark on out-of-bounds coordinates

--- app/src/test/java/com/developerxy/bnp_tic_tac_toe/TicTacToeGameBoardTests.kt
+++ app/src/test/java/com/developerxy/bnp_tic_tac_toe/TicTacToeGameBoardTests.kt
@@ -1,6 +1,7 @@
 package com.developerxy.bnp_tic_tac_toe
 
 import com.developerxy.bnp_tic_tac_toe.domain.GameBoard
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.comparables.shouldBeEqualComparingTo
 import org.junit.Before
@@ -27,5 +28,22 @@ class TicTacToeGameBoardTests {
 
         gameBoard.placeMark(mark, at = coords)
         gameBoard.markAt(coords).shouldBeEqualComparingTo(mark)
+    }
+
+    @Test
+    fun placingNewMarkOutOfGridBoundsThrowsError() {
+        val maxRow = 2; val maxCol = 2
+
+        // Place a mark in the [0..2] ranges
+        for (i in 0 until maxRow) {
+            for (j in 0 until maxCol) {
+                gameBoard.placeMark(mark = "O", at = i to j)
+            }
+        }
+
+        val outOfBoundsCoords = (maxRow + 1 to maxCol + 1)
+        shouldThrow<Exception> {
+            gameBoard.placeMark(mark = "X", at = outOfBoundsCoords)
+        }
     }
 }
