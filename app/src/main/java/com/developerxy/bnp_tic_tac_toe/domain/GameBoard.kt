a8dbbf9202e567dc3c8189f29b043d3d72012f0b
Mohammed Aouf ZOUAG <m.a.zouag@gmail.com>
2024-10-31 20:27:59 +0100

    Add logic for placing/retrieving a mark on/from the game board

--- app/src/main/java/com/developerxy/bnp_tic_tac_toe/domain/GameBoard.kt
+++ app/src/main/java/com/developerxy/bnp_tic_tac_toe/domain/GameBoard.kt
@@ -6,4 +6,11 @@ class GameBoard(
     fun isClear(): Boolean {
         return grid.all { gridRow -> gridRow.all { gridCell -> gridCell.isEmpty() } }
     }
+
+    fun placeMark(mark: String, at: Pair<Int, Int>) {
+        val (col, row) = at
+        grid[row][col] = mark
+    }
+
+    fun markAt(coords: Pair<Int, Int>): String = grid[coords.first][coords.second]
 }
