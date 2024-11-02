1592540ad5baba5b886e9affdc5bce6607db9279
Mohammed Aouf ZOUAG <m.a.zouag@gmail.com>
2024-11-02 12:18:34 +0100

    Refactor viewmodel test code

--- app/src/test/java/com/developerxy/bnp_tic_tac_toe/GameViewModelTests.kt
+++ app/src/test/java/com/developerxy/bnp_tic_tac_toe/GameViewModelTests.kt
@@ -2,8 +2,7 @@ package com.developerxy.bnp_tic_tac_toe
 
 import com.developerxy.bnp_tic_tac_toe.domain.model.GameStatus
 import com.developerxy.bnp_tic_tac_toe.ui.GameViewModel
-import junit.framework.Assert.assertEquals
-import junit.framework.Assert.assertTrue
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 
@@ -18,27 +17,29 @@ class GameViewModelTests {
 
     @Test
     fun `Initial game state is correct`() {
-        assertInitialGameStateIsCorrect()
+        gameViewModel.assertInitialGameStateIsCorrect()
     }
 
     @Test
     fun `Making a move updates the game board and toggles current player`() {
-        // Place an X initially
-        (0 to 0).let { coords ->
-            gameViewModel.makeMove(at = coords)
+        with(gameViewModel) {
+            // Place an X initially
+            (0 to 0).let { coords ->
+                makeMove(at = coords)
 
-            val gameState = gameViewModel.gameState.value
-            assertEquals("X", gameState.gameBoard.markAt(coords))
-            assertEquals("O", gameState.currentPlayer)
-        }
+                val gameState = gameState.value
+                assertEquals("X", gameState.gameBoard.markAt(coords))
+                assertEquals("O", gameState.currentPlayer)
+            }
 
-        // Place an O
-        (1 to 1).let { coords ->
-            gameViewModel.makeMove(at = coords)
+            // Place an O
+            (1 to 1).let { coords ->
+                makeMove(at = coords)
 
-            val gameState = gameViewModel.gameState.value
-            assertEquals("O", gameState.gameBoard.markAt(coords))
-            assertEquals("X", gameState.currentPlayer)
+                val gameState = gameState.value
+                assertEquals("O", gameState.gameBoard.markAt(coords))
+                assertEquals("X", gameState.currentPlayer)
+            }
         }
     }
 
@@ -49,22 +50,23 @@ class GameViewModelTests {
             X -   - O
             X -   -
          */
-        val steps = listOf(
-            { gameViewModel.makeMove(0 to 0) },
-            { gameViewModel.makeMove(0 to 1) },
-            { gameViewModel.makeMove(1 to 0) },
-            { gameViewModel.makeMove(1 to 2) },
-        )
+        with(gameViewModel) {
+            val steps = listOf(
+                { makeMove(0 to 0) },
+                { makeMove(0 to 1) },
+                { makeMove(1 to 0) },
+                { makeMove(1 to 2) },
+            )
 
-        steps.forEach {
-            it()
-            assertCurrentGameStatusMatches(GameStatus.ONGOING)
+            steps.forEach {
+                it()
+                assertCurrentGameStatusMatches(GameStatus.ONGOING)
+            }
+
+            // Final X move
+            makeMove(2 to 0)
+            assertCurrentGameStatusMatches(GameStatus.X_WON)
         }
-
-        // Final X move
-        gameViewModel.makeMove(2 to 0)
-
-        assertCurrentGameStatusMatches(GameStatus.X_WON)
     }
 
     @Test
@@ -74,22 +76,23 @@ class GameViewModelTests {
             O - X - O
               - X -
          */
-        val steps = listOf(
-            { gameViewModel.makeMove(0 to 1) },
-            { gameViewModel.makeMove(1 to 0) },
-            { gameViewModel.makeMove(1 to 1) },
-            { gameViewModel.makeMove(1 to 2) },
-        )
+        with(gameViewModel) {
+            val steps = listOf(
+                { makeMove(0 to 1) },
+                { makeMove(1 to 0) },
+                { makeMove(1 to 1) },
+                { makeMove(1 to 2) },
+            )
 
-        steps.forEach {
-            it()
-            assertCurrentGameStatusMatches(GameStatus.ONGOING)
+            steps.forEach {
+                it()
+                assertCurrentGameStatusMatches(GameStatus.ONGOING)
+            }
+
+            // Final X move
+            makeMove(2 to 1)
+            assertCurrentGameStatusMatches(GameStatus.X_WON)
         }
-
-        // Final X move
-        gameViewModel.makeMove(2 to 1)
-
-        assertCurrentGameStatusMatches(GameStatus.X_WON)
     }
 
     @Test
@@ -99,22 +102,23 @@ class GameViewModelTests {
             O -   - X
               - O - X
          */
-        val steps = listOf(
-            { gameViewModel.makeMove(0 to 2) },
-            { gameViewModel.makeMove(1 to 0) },
-            { gameViewModel.makeMove(1 to 2) },
-            { gameViewModel.makeMove(2 to 1) },
-        )
+        with(gameViewModel) {
+            val steps = listOf(
+                { makeMove(0 to 2) },
+                { makeMove(1 to 0) },
+                { makeMove(1 to 2) },
+                { makeMove(2 to 1) },
+            )
 
-        steps.forEach {
-            it()
-            assertCurrentGameStatusMatches(GameStatus.ONGOING)
+            steps.forEach {
+                it()
+                assertCurrentGameStatusMatches(GameStatus.ONGOING)
+            }
+
+            // Final X move
+            makeMove(2 to 2)
+            assertCurrentGameStatusMatches(GameStatus.X_WON)
         }
-
-        // Final X move
-        gameViewModel.makeMove(2 to 2)
-
-        assertCurrentGameStatusMatches(GameStatus.X_WON)
     }
 
     @Test
@@ -124,22 +128,23 @@ class GameViewModelTests {
               - O  - O
               -   -
          */
-        val steps = listOf(
-            { gameViewModel.makeMove(0 to 0) },
-            { gameViewModel.makeMove(1 to 1) },
-            { gameViewModel.makeMove(0 to 1) },
-            { gameViewModel.makeMove(1 to 2) },
-        )
+        with(gameViewModel) {
+            val steps = listOf(
+                { makeMove(0 to 0) },
+                { makeMove(1 to 1) },
+                { makeMove(0 to 1) },
+                { makeMove(1 to 2) },
+            )
 
-        steps.forEach {
-            it()
-            assertCurrentGameStatusMatches(GameStatus.ONGOING)
+            steps.forEach {
+                it()
+                assertCurrentGameStatusMatches(GameStatus.ONGOING)
+            }
+
+            // Final X move
+            makeMove(0 to 2)
+            assertCurrentGameStatusMatches(GameStatus.X_WON)
         }
-
-        // Final X move
-        gameViewModel.makeMove(0 to 2)
-
-        assertCurrentGameStatusMatches(GameStatus.X_WON)
     }
 
     @Test
@@ -149,22 +154,23 @@ class GameViewModelTests {
             X - X - X
               - O -
          */
-        val steps = listOf(
-            { gameViewModel.makeMove(1 to 0) },
-            { gameViewModel.makeMove(0 to 1) },
-            { gameViewModel.makeMove(1 to 1) },
-            { gameViewModel.makeMove(2 to 1) },
-        )
+        with(gameViewModel) {
+            val steps = listOf(
+                { makeMove(1 to 0) },
+                { makeMove(0 to 1) },
+                { makeMove(1 to 1) },
+                { makeMove(2 to 1) },
+            )
 
-        steps.forEach {
-            it()
-            assertCurrentGameStatusMatches(GameStatus.ONGOING)
+            steps.forEach {
+                it()
+                assertCurrentGameStatusMatches(GameStatus.ONGOING)
+            }
+
+            // Final X move
+            makeMove(1 to 2)
+            assertCurrentGameStatusMatches(GameStatus.X_WON)
         }
-
-        // Final X move
-        gameViewModel.makeMove(1 to 2)
-
-        assertCurrentGameStatusMatches(GameStatus.X_WON)
     }
 
     @Test
@@ -174,22 +180,23 @@ class GameViewModelTests {
             O - O -
             X - X - X
          */
-        val steps = listOf(
-            { gameViewModel.makeMove(2 to 0) },
-            { gameViewModel.makeMove(1 to 0) },
-            { gameViewModel.makeMove(2 to 1) },
-            { gameViewModel.makeMove(1 to 1) },
-        )
+        with(gameViewModel) {
+            val steps = listOf(
+                { makeMove(2 to 0) },
+                { makeMove(1 to 0) },
+                { makeMove(2 to 1) },
+                { makeMove(1 to 1) },
+            )
 
-        steps.forEach {
-            it()
-            assertCurrentGameStatusMatches(GameStatus.ONGOING)
+            steps.forEach {
+                it()
+                assertCurrentGameStatusMatches(GameStatus.ONGOING)
+            }
+
+            // Final X move
+            makeMove(2 to 2)
+            assertCurrentGameStatusMatches(GameStatus.X_WON)
         }
-
-        // Final X move
-        gameViewModel.makeMove(2 to 2)
-
-        assertCurrentGameStatusMatches(GameStatus.X_WON)
     }
 
     @Test
@@ -199,22 +206,23 @@ class GameViewModelTests {
             O - X -
             O -   - X
          */
-        val steps = listOf(
-            { gameViewModel.makeMove(0 to 0) },
-            { gameViewModel.makeMove(1 to 0) },
-            { gameViewModel.makeMove(1 to 1) },
-            { gameViewModel.makeMove(2 to 0) },
-        )
+        with(gameViewModel) {
+            val steps = listOf(
+                { makeMove(0 to 0) },
+                { makeMove(1 to 0) },
+                { makeMove(1 to 1) },
+                { makeMove(2 to 0) },
+            )
 
-        steps.forEach {
-            it()
-            assertCurrentGameStatusMatches(GameStatus.ONGOING)
+            steps.forEach {
+                it()
+                assertCurrentGameStatusMatches(GameStatus.ONGOING)
+            }
+
+            // Final X move
+            makeMove(2 to 2)
+            assertCurrentGameStatusMatches(GameStatus.X_WON)
         }
-
-        // Final X move
-        gameViewModel.makeMove(2 to 2)
-
-        assertCurrentGameStatusMatches(GameStatus.X_WON)
     }
 
     @Test
@@ -224,40 +232,67 @@ class GameViewModelTests {
             O - X -
             X -   - O
          */
-        val steps = listOf(
-            { gameViewModel.makeMove(0 to 2) },
-            { gameViewModel.makeMove(1 to 0) },
-            { gameViewModel.makeMove(1 to 1) },
-            { gameViewModel.makeMove(2 to 2) },
-        )
+        with(gameViewModel) {
+            val steps = listOf(
+                { makeMove(0 to 2) },
+                { makeMove(1 to 0) },
+                { makeMove(1 to 1) },
+                { makeMove(2 to 2) },
+            )
 
-        steps.forEach {
-            it()
-            assertCurrentGameStatusMatches(GameStatus.ONGOING)
+            steps.forEach {
+                it()
+                assertCurrentGameStatusMatches(GameStatus.ONGOING)
+            }
+
+            // Final X move
+            makeMove(2 to 0)
+            assertCurrentGameStatusMatches(GameStatus.X_WON)
         }
-
-        // Final X move
-        gameViewModel.makeMove(2 to 0)
-
-        assertCurrentGameStatusMatches(GameStatus.X_WON)
     }
 
     @Test
     fun `Test draw`() {
+        gameViewModel.apply {
+            simulateDraw()
+            assertCurrentGameStatusMatches(GameStatus.DRAW)
+        }
+    }
+
+    @Test
+    fun `Placing a mark on an already occupied cell does nothing`() {
+        with(gameViewModel) {
+            assertCurrentPlayerIs("X")
+            makeMove(0 to 0)
+            assertCurrentPlayerIs("O")
+            makeMove(0 to 0)
+            assertCurrentPlayerIs("O")
+        }
+    }
+
+    @Test
+    fun `Restarting the game resets its state correctly`() {
+        with(gameViewModel) {
+            restartGame()
+            assertInitialGameStateIsCorrect()
+        }
+    }
+
+    private fun GameViewModel.simulateDraw() {
         /*
             X - O - X
             O - O - X
             X - X - O
          */
         val steps = listOf(
-            { gameViewModel.makeMove(0 to 0) },
-            { gameViewModel.makeMove(0 to 1) },
-            { gameViewModel.makeMove(0 to 2) },
-            { gameViewModel.makeMove(1 to 0) },
-            { gameViewModel.makeMove(1 to 2) },
-            { gameViewModel.makeMove(1 to 1) },
-            { gameViewModel.makeMove(2 to 0) },
-            { gameViewModel.makeMove(2 to 2) },
+            { makeMove(0 to 0) },
+            { makeMove(0 to 1) },
+            { makeMove(0 to 2) },
+            { makeMove(1 to 0) },
+            { makeMove(1 to 2) },
+            { makeMove(1 to 1) },
+            { makeMove(2 to 0) },
+            { makeMove(2 to 2) },
         )
 
         steps.forEach {
@@ -266,42 +301,6 @@ class GameViewModelTests {
         }
 
         // Final X move
-        gameViewModel.makeMove(2 to 1)
-
-        assertCurrentGameStatusMatches(GameStatus.DRAW)
-    }
-
-    @Test
-    fun `Placing a mark on an already occupied cell does nothing`() {
-        assertCurrentPlayerIs("X")
-        gameViewModel.makeMove(0 to 0)
-        assertCurrentPlayerIs("O")
-        gameViewModel.makeMove(0 to 0)
-        assertCurrentPlayerIs("O")
-    }
-
-    @Test
-    fun `Restarting the game resets its state correctly`() {
-        gameViewModel.restartGame()
-        assertInitialGameStateIsCorrect()
-    }
-
-    private fun assertCurrentGameStatusMatches(gameStatus: GameStatus) {
-        val gameState = gameViewModel.gameState.value
-        assertEquals(gameStatus, gameState.status)
-    }
-
-    private fun assertCurrentPlayerIs(player: String) {
-        val gameState = gameViewModel.gameState.value
-        assertEquals(player, gameState.currentPlayer)
-    }
-
-    private fun assertInitialGameStateIsCorrect() {
-        val gameState = gameViewModel.gameState.value
-        gameState.apply {
-            assertEquals("X", currentPlayer)
-            assertTrue(gameBoard.isClear())
-            assertEquals(GameStatus.ONGOING, status)
-        }
+        makeMove(2 to 1)
     }
 }
